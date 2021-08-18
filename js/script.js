6c18888bc1a26d603788b1afb40e5a1731bd2d89
Snake <tsuchinoko.nya@gmail.com>
2021-08-18 18:15:56 +0600

    optimized code

--- js/script.js
+++ js/script.js
@@ -7,8 +7,7 @@ const personalMovieDB = {
     privat: Boolean
 };
 
-let lastFilm = personalMovieDB.movies,
-    lastFilm2 = personalMovieDB.movies;
+let lastFilm, lastFilm2 = personalMovieDB.movies;
 lastFilm = prompt('Один из последнних просмотренных фильмов', 'Logan');
 personalMovieDB.movies[lastFilm] = prompt('На сколько оцените его?', '8.1');
 lastFilm2 = prompt('Один из последнних просмотренных фильмов', 'Konan');
