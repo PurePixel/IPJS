be36743a06cbb3bd66bb4d98c9b4178558c8b886
Snake <tsuchinoko.nya@gmail.com>
2021-08-22 11:44:49 +0600

    string methods

---
+++ js/script3.js
@@ -0,0 +1,12 @@
+ const logg = "Of all the things I lost I miss my mind the most";
+
+ console.log(logg.slice(3, 5));
+ console.log(logg.substring(3, 5));
+ console.log(logg.substr(3, 5));
+
+const num = 12.4;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
